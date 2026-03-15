a935adf24a5c333dc838bdddf1bcd7a898be3cff
madhubadam <badammadhu12@gmail.com>
2026-03-15 11:05:40 +0530

    added full square,reverse right problems

--- count_numbers.py
+++ count_numbers.py
@@ -2,7 +2,7 @@ c=0
 n=123434492992
 while n>0:
     n=n//10
-    c=c+1
-    if n==0:
+    
+    if n:
         break
 print(c)
---
+++ reverse_string.py
@@ -0,0 +1,5 @@
+name=input('enter the name')
+rev=''
+for i in name:
+    rev=rev+i
+print(rev)
